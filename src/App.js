92dbeb669f4950a77523ca9a06d61d9a34a24d19
Juan-DV-Duran <55993592+Juan-DV-Duran@users.noreply.github.com>
2020-01-28 16:01:59 -0600

    Filling out initial info

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import React from "react";
 import "./styles.css";
 import "./Dashboard.js";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <h1>Howdy</h1>
-      <h2>Rebellion in the Rim Tracker!</h2>
+    <Router>
+      <div className="App">
+        <nav className="App-header">
+          <ul className="Nav">
+            <li>
+              <Link to="/" className="whiteLink">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/safetyofficer" className="whiteLink">
+                Safety Officer
+              </Link>
+            </li>
+            <li>
+              <Link to="/employee" className="whiteLink">
+                Employee
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/safetyofficer">
+            <About />
+          </Route>
+          <Route path="/employee">
+            <About id={3} />
+          </Route>
+          <Route path="/">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+function About() {
+  return (
+    <div className="About">
+      <h2 className="ritr">Rebellion in the Rim Campaign Tracker</h2>
+      <p>
+        Rebellion in the Rim Campaign Tracker is a personal project of mine to
+        better track and manage the campaign for my local group.
+      </p>
     </div>
   );
 }
+
+export default App;
